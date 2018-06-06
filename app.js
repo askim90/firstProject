5825ca3ffaa87f752fc0ee3247a3bf540db7de1d
askim90 <lkraussl@gmail.com>
2018-06-06 04:29:54 -0700

    made Show All button

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 $(document).ready(function() {
     $('.setData').on('click', function() {
-        let textFieldValue = $('.textField').val();
+        let textFieldValue = $('.name').val();
+        let textareaValue = $('textarea').val();
         //$('.debug').text(textFieldValue);
-
-        localStorage.setItem('myFormTextData', textFieldValue);
+        localStorage.setItem(textFieldValue, textareaValue);
     });
 
     $('.getData').on('click', function() {
-        let retrievedData = localStorage.getItem('myFormTextData');
-        $('.debug').text(retrievedData);
+        let textFieldValue = $('.name').val();
+        let retrievedData = localStorage.getItem(textFieldValue);
+        $('.output').text(retrievedData);
     });
 
+    $('.showAll').click(function() {
+        let keys = [];
+        
+        for (let i = 0; i < localStorage.length; i++) {
+            keys.push(localStorage.key(i));
+        }
+
+        keys.sort();
+        $('#display').html('');
+        
+        keys.forEach(e => {
+            let code = localStorage.getItem(e);
+            $('#display').append(`<li>${e}</li>`);
+            $('#display').append(`<pre><code>${code}</code></pre>`);
+        });
+    });
     // $('.textField').on('keyup', function() {
     //     let textFieldValue = $('.textField').val();
     //     $('.debug').text(textFieldValue);
